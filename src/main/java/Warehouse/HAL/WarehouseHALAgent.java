ce51120deda4472f0d112c68429b798931dcd863
junfeng_lab205 <junfeng_pan96@qq.com>
2019-03-19 19:58:22 +0800

    1. 增加SocketClient类 实现与 SocketServer 通信能力 2. 定义了 仓库Agent与硬件之间的通信协议 3. 移动SocketServer与SocketClient，整合至 MySocket包中

---
+++ src/main/java/Warehouse/HAL/WarehouseHALAgent.java
@@ -0,0 +1,16 @@
+package Warehouse.HAL;
+
+/**
+ * 仓库硬件适配层，提供统一接口函数。具体硬件操作，通过socket server转发至对应socket client处理。.
+ *
+ * @author <a href="mailto:junfeng_pan96@qq.com">junfeng</a>
+ * @version 1.0.0.0
+ * @since 1.8
+ */
+
+
+public class WarehouseHAL {
+    private static class InstanceHolder{
+        private final static WarehouseHAL = 
+    }
+}
