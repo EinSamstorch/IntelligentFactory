9b76c57879879572a236dd7599ad66d4e2c955a0
junfeng <junfeng_pan96@qq.com>
2019-08-28 17:16:28 +0800

    fix bug
    hal 获取 response时 增加 sleep 释放CPU权限

--- src/main/java/machines/real/commons/hal/BaseHal.java
+++ src/main/java/machines/real/commons/hal/BaseHal.java
@@ -87,6 +87,11 @@ public class BaseHal {
             if (response != null) {
                 return response;
             }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
