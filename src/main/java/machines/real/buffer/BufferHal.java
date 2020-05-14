b639160299d17702b5b3d988cbbdee69156a16f2
junfeng <junfeng_pan96@qq.com>
2020-05-14 10:47:29 +0800

    feat: update BufferHal
    更新为最新的命令模式

--- src/main/java/machines/real/buffer/BufferHal.java
+++ src/main/java/machines/real/buffer/BufferHal.java
@@ -4,19 +4,7 @@ import machines.real.commons.hal.BaseHal;
 
 public interface BufferHal extends BaseHal {
 
-  /**
-   * 入料.
-   *
-   * @param bufferNo 工位台编号
-   * @return 成功true, 否则false
-   */
-  boolean importItem(int bufferNo);
-
-  /**
-   * 出料.
-   *
-   * @param bufferNo 工位台编号
-   * @return 成功true, 否则false
-   */
-  boolean exportItem(int bufferNo);
+  String CMD_EXPORT_ITEM = "export_item";
+  String CMD_IMPORT_ITEM = "import_item";
+  String FIELD_BUFFER_NO = "buffer_no";
 }
