77ccea03ed6a0c8836c25011384a97bec92ffdbe
junfeng <junfeng_pan96@qq.com>
2020-05-14 16:23:46 +0800

    feat: remove redis support
    精简功能

--- src/main/java/machines/real/commons/behaviours/cycle/CheckHalState.java
+++ src/main/java/machines/real/commons/behaviours/cycle/CheckHalState.java
@@ -3,8 +3,6 @@ package machines.real.commons.behaviours.cycle;
 import commons.BaseAgent;
 import jade.core.behaviours.TickerBehaviour;
 import machines.real.commons.hal.BaseHalImpl;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 
 /**
  * 检查HAL是否在线，修改agent在线状态.
@@ -18,8 +16,6 @@ public class CheckHalState<T extends BaseAgent, H extends BaseHalImpl> extends T
 
   private T baseAgent;
   private H hal;
-  private boolean redisEnable = false;
-  private JedisPool pool;
 
   /**
    * Construct a <code>TickerBehaviour</code> that call its
@@ -34,14 +30,6 @@ public class CheckHalState<T extends BaseAgent, H extends BaseHalImpl> extends T
     this.hal = hal;
   }
 
-  public void setRedisEnable(boolean redisEnable) {
-    this.redisEnable = redisEnable;
-  }
-
-  public void setPool(JedisPool pool) {
-    this.pool = pool;
-  }
-
   @Override
   protected void onTick() {
     boolean online = false;
@@ -49,10 +37,5 @@ public class CheckHalState<T extends BaseAgent, H extends BaseHalImpl> extends T
       online = true;
     }
     baseAgent.setAgentOnline(online);
-    if (redisEnable) {
-      Jedis jedis = pool.getResource();
-      jedis.set(baseAgent.getLocalName(), online ? "online" : "offline");
-      jedis.close();
-    }
   }
 }
