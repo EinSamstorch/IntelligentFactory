1906ac915142cf7d0d8e1f42947216973d160959
EinSamstorch <einsamstorch@gmail.com>
2020-10-19 20:01:10 +0800

    fix: change sql.xml

--- src/main/java/commons/tools/db/MysqlJdbc.java
+++ src/main/java/commons/tools/db/MysqlJdbc.java
@@ -15,10 +15,9 @@ import java.util.Map;
  */
 public class MysqlJdbc {
 
-  /**
-   * 数据库连接.
-   */
+  /** 数据库连接. */
   protected Connection con;
+
   private String url;
   private String user;
   private String password;
@@ -29,25 +28,28 @@ public class MysqlJdbc {
    * @param mysqlInfo Map[String, String] 包含 mysql_ip, mysql_port, mysql_db, mysql_user 字段
    */
   public MysqlJdbc(Map<String, String> mysqlInfo) {
-    url = String.format("jdbc:mysql://%s:%s/%s?useUnicode=true&characterEncoding=utf-8",
-        mysqlInfo.get("mysql_ip"),
-        mysqlInfo.get("mysql_port"),
-        mysqlInfo.get("mysql_db"));
+    url =
+        String.format(
+            "jdbc:mysql://%s:%s/%s?useUnicode=true&characterEncoding=utf-8",
+            mysqlInfo.get("mysql_ip"), mysqlInfo.get("mysql_port"), mysqlInfo.get("mysql_db"));
     user = mysqlInfo.get("mysql_user");
     password = mysqlInfo.get("mysql_pwd");
   }
 
   protected void connect() {
     try {
+      if (this.con != null) {
+        return;
+      }
       String driver = "com.mysql.jdbc.Driver";
       Class.forName(driver);
       this.con = DriverManager.getConnection(url, user, password);
     } catch (ClassNotFoundException e) {
-      //数据库驱动类异常处理
+      // 数据库驱动类异常处理
       LoggerUtil.db.error("无法连接MYSQL驱动");
       e.printStackTrace();
     } catch (Exception e) {
-      //数据库连接失败异常处理
+      // 数据库连接失败异常处理
       e.printStackTrace();
     }
   }
@@ -61,5 +63,4 @@ public class MysqlJdbc {
       }
     }
   }
-
 }
